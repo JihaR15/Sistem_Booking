ef6889ac8e1ea439b17569ad1d763ab8c4b4b1e7
Bads171222 <ardha54312@gmail.com>
2023-09-13 20:20:19 +0700

    tambahan Booking.java

--- Sistem Booking Hotel/src/Booking.java
+++ Sistem Booking Hotel/src/Booking.java
@@ -7,6 +7,8 @@ public class Booking {
         String[] kamar = {"single", "Double", "Suite"};
         int [] harga = {50000, 80000, 150000};
         System.out.println("Silakan pilih jenis kamar yang anda inginkan:");
-
+        System.out.println("1. " + kamar[0] + " - Rp." + harga[0] + "/malam");
+        System.out.println("2. " + kamar[1] + " - Rp." + harga[1] + "/malam");
+        System.out.println("3. " + kamar[2] + " - Rp." + harga[2] + "/malam");
     }
 }
