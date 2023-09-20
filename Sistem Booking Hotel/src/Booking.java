953cc6a1358a1ab11c57757f8cb594c31f724fb7
“JihaR15 <jiharamdhan97@gmail.com>
2023-09-20 19:27:38 +0700

    karena belum boleh pake array, jadi diganti

--- Sistem Booking Hotel/src/Booking.java
+++ Sistem Booking Hotel/src/Booking.java
@@ -2,25 +2,23 @@ import java.util.Scanner;
 
 public class Booking {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
-        String[] kamar = {"Single", "Double", "Suite"};
-        int [] harga = {50000, 80000, 150000};
-        System.out.println("Silakan pilih jenis kamar yang anda inginkan:");
 
-        System.out.println("1. " + kamar[0] + " - Rp." + harga[0] + "/malam");
-        System.out.println("2. " + kamar[1] + " - Rp." + harga[1] + "/malam");
-        System.out.println("3. " + kamar[2] + " - Rp." + harga[2] + "/malam");
+        String kamar = "Suite", nama ;
+        int harga = 50000, perMalam;
 
-        System.out.print("Pilihan Anda (1/2/3): ");
-        int pilihanKamar = sc.nextInt();
+        System.out.println("\nKamar yang anda plih yaitu: ");
+        System.out.println("1. " + kamar + " - Rp." + harga + "/malam");
 
-        System.out.print("Berapa malam Anda ingin menginap: ");
-        int perMalam = sc.nextInt();
+        System.out.print("\nAtas nama siapa: ");
+        nama = sc.nextLine();
 
-        int totalHarga = harga[pilihanKamar - 1] * perMalam; // -1 karna angka dimulai dari 0
-        System.out.println("Total harga untuk " + kamar[pilihanKamar - 1] + " selama " + perMalam + " malam adalah Rp." + totalHarga);
+        System.out.print("Halo " + nama +", Berapa malam Anda ingin menginap: ");
+        perMalam = sc.nextInt();
 
-
+        int totalHarga = harga * perMalam;
+        System.out.println("Total harga untuk " + kamar + " selama " + perMalam + " malam adalah Rp." + totalHarga);
+        System.out.println(" ");
     }
 }
