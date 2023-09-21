c98d54e7b5c24fc3f39e97a6313c93fcaa96c700
Fatikah002 <sfatikah2005@gmail.com>
2023-09-21 09:40:33 +0700

    tambah keterangan

--- Sistem Booking Hotel/src/Booking.java
+++ Sistem Booking Hotel/src/Booking.java
@@ -5,8 +5,24 @@ public class Booking {
 
         Scanner sc = new Scanner(System.in);
 
+        String kmr1 = "Single";
+        String kmr2 = "Double";
+        String kmr3 = "Suite";
+        int hargaKamar1 = 50000;
+        int hargaKamar2 = 80000;
+        int hargaKamar3 = 100000;
+        String kamar1 = kmr1 +"\t"+ hargaKamar1;
+        String kamar2 = kmr2 +"\t"+ hargaKamar2;
+        String kamar3 = kmr3 +"\t\t" +hargaKamar3;
+
+        System.out.println("\n==== KATEGORI KAMAR ====");
+        System.out.println("| "+kamar1+"  \t|");
+        System.out.println("| "+kamar2+"  \t|");
+        System.out.println("| "+kamar3+"\t|");
+        System.out.println("========================");
+
         String kamar = "Suite", nama ;
-        int harga = 50000, perMalam;
+        int harga = 100000, perMalam;
 
         System.out.println("\nKamar yang anda plih yaitu: ");
         System.out.println("1. " + kamar + " - Rp." + harga + "/malam");
