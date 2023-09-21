dd913a97ae0dc52927732a89448f253df0b80a99
“JihaR15 <jiharamdhan97@gmail.com>
2023-09-21 12:05:01 +0700

    Mempersingkat & sedikit perubahan

--- Sistem Booking Hotel/src/Booking.java
+++ Sistem Booking Hotel/src/Booking.java
@@ -5,27 +5,19 @@ public class Booking {
 
         Scanner sc = new Scanner(System.in);
 
-        String kmr1 = "Single";
-        String kmr2 = "Double";
-        String kmr3 = "Suite";
-        int hargaKamar1 = 50000;
-        int hargaKamar2 = 80000;
-        int hargaKamar3 = 100000;
-        String kamar1 = kmr1 +"\t"+ hargaKamar1;
-        String kamar2 = kmr2 +"\t"+ hargaKamar2;
-        String kamar3 = kmr3 +"\t\t" +hargaKamar3;
+        String kmr1 = "Single", kmr2 = "Double", kmr3 = "Suite", nama;
+        int hargaKmr1 = 50000, hargaKmr2 = 80000, hargaKmr3 = 100000, perMalam;
 
-        System.out.println("\n==== KATEGORI KAMAR ====");
-        System.out.println("| "+kamar1+"  \t|");
-        System.out.println("| "+kamar2+"  \t|");
-        System.out.println("| "+kamar3+"\t|");
-        System.out.println("========================");
 
-        String kamar = "Suite", nama ;
-        int harga = 100000, perMalam;
+        System.out.println("\n====== KATEGORI KAMAR ======");
+        System.out.println("| "+ kmr1 + " - Rp." + hargaKmr1 + "/malam  |");
+        System.out.println("| "+ kmr2 + " - Rp." + hargaKmr2 + "/malam  |");
+        System.out.println("| "+ kmr3 + " - Rp." + hargaKmr3 + "/malam  |");
+ 
+        System.out.println("============================");
 
         System.out.println("\nKamar yang anda plih yaitu: ");
-        System.out.println("1. " + kamar + " - Rp." + harga + "/malam");
+        System.out.println("1. " + kmr1 + " - Rp." + hargaKmr1 + "/malam");
 
         System.out.print("\nAtas nama siapa: ");
         nama = sc.nextLine();
@@ -33,8 +25,8 @@ public class Booking {
         System.out.print("Halo " + nama +", Berapa malam Anda ingin menginap: ");
         perMalam = sc.nextInt();
 
-        int totalHarga = harga * perMalam;
-        System.out.println("Total harga untuk " + kamar + " selama " + perMalam + " malam adalah Rp." + totalHarga);
+        int totalHarga = hargaKmr1 * perMalam;
+        System.out.println("Total harga untuk " + kmr1 + " selama " + perMalam + " malam adalah Rp." + totalHarga);
         System.out.println(" ");
     }
 }
