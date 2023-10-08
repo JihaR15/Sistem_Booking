83ebff4e7fd16d73ad29361dfe15692affd06c8a
Fatikah002 <sfatikah2005@gmail.com>
2023-10-08 12:58:47 +0700

    menambah fitur customer

--- Sistem Booking Hotel/src/Booking.java
+++ Sistem Booking Hotel/src/Booking.java
@@ -72,7 +72,62 @@ public class Booking {
                     break;
             }
             
-        } else {
+        }else if ((username.equals("customer")) && (password.equals("customer1"))) {
+            System.out.println("------- Selamat Datang, " + username + "! -------");
+             System.out.println("\n=================================================");
+             System.out.println("\t\t    MENU \t\t");
+             System.out.println("=================================================");
+             System.out.print("1. Informasi Login"); 
+             System.out.println("\t\t5. Histori Pemesanan");
+             System.out.print("2. Cek Ketersediaan Kamar");  
+             System.out.println("\t6. Logout");
+             System.out.print("3. Pemesanan Kamar");
+             System.out.println("\t\t7. Tutup Program");
+             System.out.println("4. Pembayaran");
+             System.out.println("=================================================");
+             System.out.print("Silahkan pilih menu (1-7)     : ");
+            int menu = sc.nextInt();
+            switch (menu) {
+                 case 1:
+                     System.out.println("=================================================");
+                     System.out.println("\t\t  Informasi Login \t\t");
+                     System.out.println("=================================================");
+                     break;
+                 case 2:
+                     System.out.println("=================================================");
+                     System.out.println("\t     Cek Ketersediaan Kamar \t\t");
+                     System.out.println("=================================================");
+                     break;
+                 case 3:
+                     System.out.println("=================================================");
+                     System.out.println("\t\t  Pemesanan Kamar \t\t");
+                     System.out.println("=================================================");
+                     break;
+                 case 4:
+                     System.out.println("=================================================");
+                     System.out.println("\t\t  Pembayaran \t\t");
+                     System.out.println("=================================================");
+                     break;
+                 case 5:
+                     System.out.println("\n=================================================");
+                     System.out.println("\t\t  Histori Pemesanan \t\t");
+                     System.out.println("=================================================");
+                     break;
+                 case 6:
+                     System.out.println("=================================================");
+                     System.out.println("\t\t  Logout \t\t");
+                     System.out.println("=================================================");
+                     break;
+                 case 7:
+                     System.out.println("=================================================");
+                     System.out.println("   Terima Kasih Telah Menggunakan Layanan Kami   ");
+                     System.out.println("=================================================");
+ 
+                 default:
+                     break;
+             }
+             
+        }else {
             System.out.println("-------- Login Gagal -------");
 
         }
