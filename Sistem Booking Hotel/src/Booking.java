8cd25f65652c011bcdfc7f55bafa40ae6f328ecc
“JihaR15 <jiharamdhan97@gmail.com>
2023-10-08 10:34:12 +0700

    Nested If untuk menu admin

--- Sistem Booking Hotel/src/Booking.java
+++ Sistem Booking Hotel/src/Booking.java
@@ -11,23 +11,67 @@ public class Booking {
         username =sc.nextLine();
         System.out.print("Password  : ");
         password = sc.nextLine();
-        if (username.equals("admin") && password.equals("12345")) {
-            System.out.println("------- Login Sukses -------");
-            System.out.println("\n=========================================");
-            System.out.println("|\t       MENU UTAMA \t\t|");
-            System.out.println("=========================================");
-            System.out.print("    1. Profil User");
-            System.out.println("\t5. -");
-            System.out.print("    2. Check-In");
-            System.out.println("\t\t6. -");
-            System.out.print("    3. Check-Out");
-            System.out.println("\t7. Logout");
-            System.out.print("    4. -");
+        if (username.equals("admin") && password.equals("admin")) {
+            System.out.println("------- Selamat Datang, " + username + "! -------");
+            System.out.println("\n=================================================");
+            System.out.println("\t\t    ADMIN MENU \t\t");
+            System.out.println("=================================================");
+            System.out.print(" 1. Informasi Login");
+            System.out.println("\t\t5. Histori Pemesanan");
+            System.out.print(" 2. Cek Ketersediaan Kamar");  
+            System.out.println("\t6. Daftar Nama Pengguna");
+            System.out.print(" 3. Input Kamar Baru");
+            System.out.println("\t\t7. Logout");
+            System.out.print(" 4. Input Pengguna Baru");
             System.out.println("\t\t8. Tutup Program");
-            System.out.println("=========================================");
+            System.out.println("=================================================");
             System.out.print("Silahkan pilih menu (1-8)     : ");
             int menu = sc.nextInt();
-            // dikarenakan belum boleh if level 2 jadi dipisah filenya, di MenuUtama.java
+            switch (menu) {
+                case 1:
+                    System.out.println("=================================================");
+                    System.out.println("\t\t  Informasi Login \t\t");
+                    System.out.println("=================================================");
+                    break;
+                case 2:
+                    System.out.println("=================================================");
+                    System.out.println("\t     Cek Ketersediaan Kamar \t\t");
+                    System.out.println("=================================================");
+                    break;
+                case 3:
+                    System.out.println("=================================================");
+                    System.out.println("\t\t  Input Kamar Baru \t\t");
+                    System.out.println("=================================================");
+                    break;
+                case 4:
+                    System.out.println("=================================================");
+                    System.out.println("\t\t  Input Pengguna Baru \t\t");
+                    System.out.println("=================================================");
+                    break;
+                case 5:
+                    System.out.println("\n=================================================");
+                    System.out.println("\t\t  Histori Pemesanan \t\t");
+                    System.out.println("=================================================");
+                    break;
+                case 6:
+                    System.out.println("=================================================");
+                    System.out.println("\t\t  Daftar Nama Pengguna \t\t");
+                    System.out.println("=================================================");
+                    break;
+                case 7:
+                    System.out.println("=================================================");
+                    System.out.println("\t\t  Logout \t\t");
+                    System.out.println("=================================================");
+                    break;
+                case 8:
+                    System.out.println("=================================================");
+                    System.out.println("   Terima Kasih Telah Menggunakan Layanan Kami   ");
+                    System.out.println("=================================================");
+
+                default:
+                    break;
+            }
+            
         } else {
             System.out.println("-------- Login Gagal -------");
 
