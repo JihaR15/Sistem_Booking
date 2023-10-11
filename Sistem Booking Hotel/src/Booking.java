5b973fcd1165fd9f1d6e1b3368ba9b467de6fa60
Bads171222 <ardha54312@gmail.con>
2023-10-11 13:03:57 +0700

    tambah sistem pemesanan

--- Sistem Booking Hotel/src/Booking.java
+++ Sistem Booking Hotel/src/Booking.java
@@ -102,7 +102,30 @@ public class Booking {
                      System.out.println("=================================================");
                      System.out.println("\t\t  Pemesanan Kamar \t\t");
                      System.out.println("=================================================");
+                     String[] kamar = {"Single", "Double", "Suite"};
+                     int[] harga = {50000, 80000, 150000};
+             
+                     System.out.println("Silakan pilih jenis kamar yang ingin Anda pesan:");
+                     System.out.println("1. " + kamar[0] + " - Rp." + harga[0] + "/malam");
+                     System.out.println("2. " + kamar[1] + " - Rp." + harga[1] + "/malam");
+                     System.out.println("3. " + kamar[2] + " - Rp." + harga[2] + "/malam");
+             
+                     System.out.print("Pilihan Anda (1/2/3): ");
+                     int pilihanKamar = sc.nextInt();
+             
+                     if (pilihanKamar < 1 || pilihanKamar > 3) {
+                         System.out.println("Pilihan tidak valid. Harap pilih antara 1, 2, atau 3.");
+                         break;
+                     }
+             
+                     System.out.print("Berapa malam Anda ingin menginap: ");
+                     int perMalam = sc.nextInt();
+             
+                     int totalPrice = harga[pilihanKamar - 1] * perMalam;
+                     System.out.println("Total harga untuk " + kamar[pilihanKamar - 1] + " selama " + perMalam + " malam adalah Rp." + totalPrice);
+             
                      break;
+                     
                  case 4:
                      System.out.println("=================================================");
                      System.out.println("\t\t  Pembayaran \t\t");
@@ -132,6 +155,6 @@ public class Booking {
 
         }
         
-       
+       sc.close();
     }
 }
